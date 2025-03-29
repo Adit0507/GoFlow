0bb460691a1dd632613cb4b74e03bbd8ebfeeecf
Adit0507 <adi05072002@gmail.com>
2025-03-30 02:15:42 +0530

    settin up logger service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort = "80"	
+	rpcPort = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("error connecting: ", err)
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func main() {
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
